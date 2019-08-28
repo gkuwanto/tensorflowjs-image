dcb491925996a6b5859d4b5499aa1025110991ae
Garry Kuwanto <gkuwanto@gmail.com>
2019-08-28 22:39:32 +0700

    Added custom classifier

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let net;
 
 const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
+
 
 async function app() {
   console.log('Loading mobilenet..');
@@ -10,16 +12,31 @@ async function app() {
   console.log('Sucessfully loaded model');
 
   await setupWebcam();
+
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  document.getElementById('class-a').addEventListener('click', ()=> addExample(0));
+  document.getElementById('class-b').addEventListener('click', ()=> addExample(1));
+  document.getElementById('class-c').addEventListener('click', ()=> addExample(2));
+  document.getElementById('class-d').addEventListener('click', ()=> addExample(3));
+
   while (true) {
-    const result = await net.classify(webcamElement);
+    if(classifier.getNumClasses()>0){
+      const activation = net.infer(webcamElement, 'conv_preds');
+      const result = await classifier.predictClass(activation);
+      const classes = ['You Are Doing Nothing', 'Rock', 'Paper', 'Scissors'];
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.classIndex]}\n
+        probability: ${result.confidences[result.classIndex]}
+      `;
+    } 
 
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
-
-    // Give some breathing room by waiting for the next animation frame to
-    // fire.
     await tf.nextFrame();
   }
 }
